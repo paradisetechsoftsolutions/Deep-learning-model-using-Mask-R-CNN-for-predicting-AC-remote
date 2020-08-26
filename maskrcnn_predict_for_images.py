33154155e139cc4cde52cffff891009415842951
Paradise Techosft Solutions <39157936+paradisetechsoftsolutions@users.noreply.github.com>
2020-08-26 11:54:12 +0530

    Prediction on Images

---
+++ maskrcnn_predict_for_images.py
@@ -0,0 +1,74 @@
+"""
+LIbraries used for prediction
+"""
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("./")
+print("ROOT_DIR", ROOT_DIR)
+sys.path.append(ROOT_DIR)
+
+# Import Mask RCNN
+from mrcnn import utils
+from mrcnn import visualize
+from mrcnn.visualize import display_images
+import mrcnn.model as modellib
+from mrcnn.model import log
+# import custom file module
+import custom
+
+# Directory to load saved model
+MODEL_DIR = os.path.join(ROOT_DIR, "log")
+config = custom.CustomConfig()
+
+# changes for inferencing.
+class InferenceConfig(config.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+config = InferenceConfig()
+config.display()
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+# Load weights trained on custom dataset
+custom_dataset_path =os.path.join(MODEL_DIR, "custom.h5")
+model.load_weights(custom_dataset_path, by_name=True)
+
+dataset = custom.CustomDataset()
+CUSTOM_DIR = os.path.join(ROOT_DIR, "dataset")
+dataset.load_custom(CUSTOM_DIR, "val")
+
+# Must call before using the dataset
+dataset.prepare()
+# print("final", dataset.class_names)
+# Load a random image from the images folder
+class_names = dataset.class_names
+
+# image_id = random.choice(dataset.image_ids)
+# image, image_meta, gt_class_id, gt_bbox, gt_mask = \
+#     modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
+# info = dataset.image_info[image_id]
+# print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id,
+#                                        dataset.image_reference(image_id)))
+
+# real time testing on an image
+import matplotlib.image as mpimg
+image = mpimg.imread('download.webp')
+
+# Run object detection
+results = model.detect([image], verbose=1)
+r = results[0]
+visualize.display_instances(
+							image, r['rois'], r['masks'], 
+							r['class_ids'],class_names, r['scores'])
